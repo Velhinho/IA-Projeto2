908416ec7f0266d20cd77312023bfef721b93892
Velhinho <goncalo.velhinho420@gmail.com>
2019-12-02 16:01:18 +0000

    Selecting action to learn randomly.

--- ruagomesfreiregame2sol.py
+++ ruagomesfreiregame2sol.py
@@ -8,27 +8,31 @@ class LearningAgent:
   # init
   # nS maximum number of states
   # nA maximum number of action per state
-  def __init__(self,nS,nA):
-
-    # define this function
+  def __init__(self, nS, nA):
     self.nS = nS
-    self.nA = nA
-    # define this function
-  
-  
+    self.nA = nA  
+    # A matrix with Q_table[state][action] = Q(s, a)
+    self.Q_table = [[] for state in range(nS)]
+
+  def exploration_func(self, size_range):
+    return random.randint(0, size_range - 1)
+
   # Select one action, used when learning  
   # st - is the current state        
   # aa - is the set of possible actions
   # for a given state they are always given in the same order
   # returns
   # a - the index to the action in aa
-  def selectactiontolearn(self,st,aa):
-    # define this function
+  def selectactiontolearn(self, st, aa):
     # print("select one action to learn better")
+    
+    # If this state doesnt have Q_table initialized 
+    # Initialize with a list with the size of number of actions possible
+    # For that State
+    if(len(self.Q_table[st]) == 0):
+      self.Q_table[st] = [0 for action in range(len(aa))]
 
-    a = 0
-    # define this function
-    return a
+    return self.exploration_func(len(aa))
 
   # Select one action, used when evaluating
   # st - is the current state        
@@ -36,20 +40,23 @@ class LearningAgent:
   # for a given state they are always given in the same order
   # returns
   # a - the index to the action in aa
-  def selectactiontoexecute(self,st,aa):
-    # define this function
-    a = 0
+  def selectactiontoexecute(self, st, aa):
     # print("select one action to see if I learned")
-    return a
+    max_value = max(self.Q_table[st])
+    a = self.Q_table[st].index(max_value)
 
+    return a
 
   # this function is called after every action
   # ost - original state
   # nst - next state
   # a - the index to the action taken
   # r - reward obtained
-  def learn(self,ost,nst,a,r):
-    # define this function
+  def learn(self, ost, nst, a, r):
     #print("learn something from this data")
-    
-    return
+    learn_rate = 0.01
+    gamma = 0.9
+
+    self.Q_table[ost][a] = self.Q_table[ost][a] + learn_rate \
+        * (r + gamma * max(self.Q_table[nst], default=0) - self.Q_table[ost][a])
+
